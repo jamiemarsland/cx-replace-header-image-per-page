dbc414454ec3155e89b0ccf222acfc66a1b8f067
Nick Burne <nickburne@users.noreply.github.com>
2014-06-23 10:36:21 +0100

    Update pootlepress-replace-header-image.php

--- pootlepress-replace-header-image.php
+++ pootlepress-replace-header-image.php
@@ -3,7 +3,7 @@
 Plugin Name: Canvas Extension - Replace Header Image Per Page
 Plugin URI: http://www.pootlepress.com/
 Description: An extension for WooThemes Canvas that allow you to use different header image for each post, page or product page.
-Version: 1.0.1
+Version: 1.1.0
 Author: PootlePress
 Author URI: http://pootlepress.com/
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -16,6 +16,6 @@ require_once( 'pootlepress-replace-header-image-functions.php' );
 require_once( 'classes/class-pootlepress-replace-header-image.php' );
 
 $GLOBALS['pootlepress_replace_header_image'] = new Pootlepress_Replace_Header_Image( __FILE__ );
-$GLOBALS['pootlepress_replace_header_image']->version = '1.0.1';
+$GLOBALS['pootlepress_replace_header_image']->version = '1.1.0';
 
 ?>
