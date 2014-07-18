b16b0313eb551dd889d0e6dd9cdb69e0ba6dae21
Alan Ng <jenfoong@gmail.com>
2014-07-18 17:03:57 +0800

    Add Updater

--- pootlepress-replace-header-image.php
+++ pootlepress-replace-header-image.php
@@ -14,8 +14,22 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 require_once( 'pootlepress-replace-header-image-functions.php' );
 require_once( 'classes/class-pootlepress-replace-header-image.php' );
+require_once( 'classes/class-pootlepress-updater.php');
 
 $GLOBALS['pootlepress_replace_header_image'] = new Pootlepress_Replace_Header_Image( __FILE__ );
 $GLOBALS['pootlepress_replace_header_image']->version = '1.1.0';
+
+add_action('init', 'pp_rhi_updater');
+function pp_rhi_updater()
+{
+    if (!function_exists('get_plugin_data')) {
+        include(ABSPATH . 'wp-admin/includes/plugin.php');
+    }
+    $data = get_plugin_data(__FILE__);
+    $wptuts_plugin_current_version = $data['Version'];
+    $wptuts_plugin_remote_path = 'http://www.pootlepress.com/?updater=1';
+    $wptuts_plugin_slug = plugin_basename(__FILE__);
+    new Pootlepress_Updater ($wptuts_plugin_current_version, $wptuts_plugin_remote_path, $wptuts_plugin_slug);
+}
 
 ?>
